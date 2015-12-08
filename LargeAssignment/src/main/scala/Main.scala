5a54beca73a429d244374f6d63b064fe5abcad88
Luís Cleto <luisccleto@gmail.com>
2015-12-08 17:06:30 +0100

    Add (very few) examples and some code to walk through files in a directory and parse them

--- LargeAssignment/src/main/scala/Main.scala
+++ LargeAssignment/src/main/scala/Main.scala
@@ -1,9 +1,30 @@
+import java.nio.file.{Path, Paths, Files}
+import java.util.function.{Predicate, Consumer}
+
 import ch.usi.inf.reveal.parsing.artifact._
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val artifact = ArtifactSerializer.deserializeFromFile(getClass.getResource("/10000008.json").getPath)
-
-    print(artifact.question.id)
+  implicit def toConsumer[A](function: A => Unit): Consumer[A] = new Consumer[A]() {
+    override def accept(arg: A): Unit = function.apply(arg)
   }
+  implicit def toPredicate[A](function: A => Boolean): Predicate[A] = new Predicate[A]() {
+    override def test(arg: A): Boolean = function.apply(arg)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val stormedDatasetDir = getClass.getResource(".").getPath.substring(1) //define for folder with stormed json files
+
+    var score = 0.0
+    var numfiles = 0
+    Files.walk(Paths.get(stormedDatasetDir)).filter({(f:Path) => f.toString.endsWith(".json")}).forEach({(f:Path) =>
+      val artifact = ArtifactSerializer.deserializeFromFile(f.toString)
+      score *= numfiles
+      score += artifact.question.score
+      numfiles += 1
+      score = score / numfiles
+
+      println(artifact.question.score + " - " + score)
+    })
+  }
+
 }
